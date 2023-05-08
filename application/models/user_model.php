ad0285984600bd1204553a8f029ee3048ac1f8f7
sauqi28 <riza.valasev@peruri.co.id>
2023-05-08 13:45:03 +0700

    s

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -18,13 +18,11 @@ class User_model extends CI_Model
   // }
   public function check_user($username)
   {
-    $this->db->select('id, username, category, fullname, email, password, role');
-    $this->db->from('mst_users');
-    $this->db->group_start();
-    $this->db->where('username', $username);
-    $this->db->or_where('nip', $username);
-    $this->db->or_where('email', $username);
-    $this->db->group_end();
+    $this->db->select('a.id, a.username, a.category, a.fullname, a.email, a.password, b.id as position_id, b.position_name, c.id as role_id, c.role_name, a.status');
+    $this->db->from('mst_users a');
+    $this->db->join('mst_user_position b', 'a.position = b.id', 'left');
+    $this->db->join('mst_user_role c', 'a.role = c.id', 'left');
+    $this->db->where("(a.username = '$username' OR a.nip = '$username' OR a.email = '$username')");
     $this->db->limit(1);
     $user = $this->db->get()->row();
     return $user;
