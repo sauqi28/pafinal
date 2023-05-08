2b5dc6bc9771f53f899b9e1ee1c04ebd57a47ecc
sauqi28 <riza.valasev@peruri.co.id>
2023-05-08 15:12:35 +0700

    s

--- application/views/data/tracking/navbar.php
+++ application/views/data/tracking/navbar.php
@@ -2,17 +2,8 @@
     <li class="nav-item ">
       <a class="nav-link mdi mdi-format-list-bulleted-type <?php if ($navbar == "data_user") {
                                                               echo "active";
-                                                            } ?>" href="<?php echo base_url('data_user/'); ?>">Daftar User Aktif</a>
+                                                            } ?>" href="<?php echo base_url('tracking/'); ?>">Pencarian SN</a>
     </li>
-    <li class="nav-item ">
-      <a class="nav-link mdi mdi-format-list-bulleted-type <?php if ($navbar == "data_user_nonaktif") {
-                                                              echo "active";
-                                                            } ?>" href="<?php echo base_url('data_user/non_aktif'); ?>">Daftar User Non Aktif</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link mdi mdi-account-plus <?php if ($navbar == "data_user_add") {
-                                                echo "active";
-                                              } ?>" href="<?php echo base_url('data_user/create/'); ?>">Tambah User</a>
-    </li>
+
 
   </ul>
