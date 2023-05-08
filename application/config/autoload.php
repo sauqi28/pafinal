7037210f872179f5385168901bb2383629b6719d
sauqi28 <riza.valasev@peruri.co.id>
2023-05-08 11:30:20 +0700

    s

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('database', 'session', 'encryption');
+$autoload['libraries'] = array('database', 'session', 'encrypt');
 
 
 /*
