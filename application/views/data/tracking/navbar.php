668ef74178070807ebdcc79ac11ee17fedec8fd0
sauqi28 <riza.valasev@peruri.co.id>
2023-05-08 15:13:05 +0700

    s

--- application/views/data/tracking/navbar.php
+++ application/views/data/tracking/navbar.php
@@ -1,9 +1,5 @@
   <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-    <li class="nav-item ">
-      <a class="nav-link mdi mdi-format-list-bulleted-type <?php if ($navbar == "data_user") {
-                                                              echo "active";
-                                                            } ?>" href="<?php echo base_url('tracking/'); ?>">Pencarian SN</a>
-    </li>
+
 
 
   </ul>
