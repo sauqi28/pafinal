29c4fce77e2f377a4f6677a579f0686b0322592b
sauqi28 <riza.valasev@peruri.co.id>
2023-05-09 10:19:47 +0700

    s

--- application/models/dashboard_model.php
+++ application/models/dashboard_model.php
@@ -4,7 +4,7 @@ class Dashboard_model extends CI_Model
 {
   public function grafik1()
   {
-    $query = $this->db->query("SELECT sum(tot) as total_data, report_date FROM report_1 WHERE tot IS NOT NULL GROUP BY report_date");
+    $query = $this->db->query("SELECT sum(tot) as total_data, report_date FROM report_1 WHERE tot IS NOT NULL AND year(report_date) = 2023 GROUP BY report_date");
     $result = $query->result_array();
     foreach ($result as &$row) {
       $row['report_date'] = date("d/M/y", strtotime($row['report_date']));
