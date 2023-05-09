8e4fd0948bdec74273f6a2dfbe71d3d882c0989a
sauqi28 <riza.valasev@peruri.co.id>
2023-05-09 10:12:33 +0700

    s

--- application/models/dashboard_model.php
+++ application/models/dashboard_model.php
@@ -1,6 +1,6 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
 
-class Dashboard extends CI_Model
+class Dashboard_model extends CI_Model
 {
   public function grafik1()
   {
