1dbd998485c3613b9391755224a7bb94b5cf4adb
sauqi28 <riza.valasev@peruri.co.id>
2023-05-09 10:13:29 +0700

    s

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,48 +1,32 @@
 <?php $this->load->view('project/element/header'); ?>
+<script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
 <?php $this->load->view('project/element/sidebar'); ?>
 
 <div class="page-wrapper">
 
-  <html>
-
-  <head>
-    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
-    <style>
-      #chart {
-        height: 500px;
-        width: 100%;
-      }
-    </style>
-  </head>
-
-  <body>
-    <div id="chart"></div>
-    <script>
-      var options = {
-        chart: {
-          type: 'bar'
-        },
-        series: [{
-          name: 'Total Data',
-          data: [
-            <?php
-            foreach ($data as $row) {
-              echo '{ x: "' . $row['report_date'] . '", y: ' . $row['total_data'] . ' },';
-            }
-            ?>
-          ]
-        }],
-        xaxis: {
-          type: 'datetime',
-        },
-      }
-      var chart = new ApexCharts(document.querySelector("#chart"), options);
-      chart.render();
-    </script>
-  </body>
-
-  </html>
-
+  <div id="chart"></div>
+  <script>
+    var options = {
+      chart: {
+        type: 'bar'
+      },
+      series: [{
+        name: 'Total Data',
+        data: [
+          <?php
+          foreach ($data as $row) {
+            echo '{ x: "' . $row['report_date'] . '", y: ' . $row['total_data'] . ' },';
+          }
+          ?>
+        ]
+      }],
+      xaxis: {
+        type: 'datetime',
+      },
+    }
+    var chart = new ApexCharts(document.querySelector("#chart"), options);
+    chart.render();
+  </script>
 
 </div>
 
