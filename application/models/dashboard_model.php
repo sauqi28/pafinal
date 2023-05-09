d7bf0a2ed2fa363f7da379c6db66c08332c0c0fe
sauqi28 <riza.valasev@peruri.co.id>
2023-05-09 10:34:43 +0700

    s

--- application/models/dashboard_model.php
+++ application/models/dashboard_model.php
@@ -4,10 +4,10 @@ class Dashboard_model extends CI_Model
 {
   public function grafik1()
   {
-    $query = $this->db->query("SELECT sum(tot) as total_data, month(report_date) as bulan, year(report_date) as tahun FROM report_1 WHERE tot IS NOT NULL  GROUP BY month(report_date), year(report_date)");
+    $query = $this->db->query("SELECT sum(tot) as total_data, month(report_date) as bulan, year(report_date) as tahun FROM report_1 WHERE tot IS NOT NULL  GROUP BY month(report_date), year(report_date) ORDER BY year(report_date) ASC, month(report_date) ASC");
     $result = $query->result_array();
     foreach ($result as &$row) {
-      $row['report_date'] = date("M/y", strtotime($row['bulan'] . "/01/" . $row['tahun']));
+      $row['report_date'] = date("M/y", strtotime($row['tahun'] . '-' . $row['bulan'] . '-01'));
     }
     return $result;
   }
