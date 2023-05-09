ac26da3328fba02924721dd7618e049bcae4c2ff
sauqi28 <riza.valasev@peruri.co.id>
2023-05-09 10:40:00 +0700

    s

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -165,13 +165,13 @@
                           data: [
                             <?php
                             foreach ($data as $row) {
-                              echo '{ x: "' . $row['bulan'] . '/' . $row['tahun'] . '", y: ' . $row['total_data'] . ' },';
+                              echo '{ x: "' . $row['report_date'] . '", y: ' . $row['total_data'] . ' },';
                             }
                             ?>
                           ]
                         }],
                         xaxis: {
-                          type: 'category',
+                          type: 'category'
                         },
                       }
                       var chart = new ApexCharts(document.querySelector("#chart"), options);
