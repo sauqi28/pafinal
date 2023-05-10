156bc9f2718665493adc868b95a0a7df84c25781
sauqi28 <riza.valasev@peruri.co.id>
2023-05-10 14:21:37 +0700

    s

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -277,20 +277,7 @@
                 </div><!--end card-header-->
                 <div class="card-body">
                   <div class="">
-                    <div class="card overflow-hidden">
-                      <div class="card-body">
-                        <div class="pt-3">
-                          <h3 class="text-dark text-center font-24 fw-bold line-height-lg">Data Mesin
 
-                          </h3>
-
-                          <div class="text-center py-3 mb-3">
-                            <a href="#" class="btn btn-success">Report</a>
-                          </div>
-                          <!-- <img src="assets/images/small/business.png" alt="" class="img-fluid px-3 mb-2"> -->
-                        </div>
-                      </div>
-                    </div>
                     <div id="chart2"></div>
                     <script>
                       var options = {
